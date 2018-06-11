b2c68d943d4a7aa5a81cdfc12d8158350cb64a80
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-06-11 17:05:22 -0500

    shrink binary

--- test/hello.s
+++ test/hello.s
@@ -1,23 +1,23 @@
-.data
+.section .rodata
 
+msg: .ascii "Hello World!\n"
+
+.equ msg_len, .-msg
 .equ sys_write, 4
 .equ sys_exit, 1
 .equ stdout, 1
 
-msg: .ascii "Hello World!\n"
-.equ msg_len, .-msg
-
-.text
+.section .text
 
 .global _start
 
 _start:
-  mov $sys_write, %rax
+  mov $4, %rax
   mov $stdout, %rdi
   mov $msg, %rsi
   mov $msg_len, %rdx
   syscall
 
   mov $sys_exit, %rax
-  mov $0, %rdi
+  xor %rdi, %rdi
   syscall
